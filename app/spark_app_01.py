711752b53b269982b21e4ad5596f67b8d3b73898
lin1000 <lin1000@gmail.com>
2018-11-28 16:46:59 +0800

    test

--- app/spark_app_01.py
+++ app/spark_app_01.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     t1.printSchema()
 
     t1DF = t1.select(explode("documents").alias("tweet"))
-    t1DF.select("tweet.Author","tweet.FollowersCount").write.csv("/output/t1DF", encoding="utf-8")
+    t1DF.select("tweet.Author","tweet.FollowersCount").write.__version__
 
     # namesDF = tweetDF.select("firstName","lastName")
     # namesDF.write.option("header","true").csv(sys.argv[2])
