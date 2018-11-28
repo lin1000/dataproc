4463f81f14f161c119af1446cccfc8d259c299ae
lin1000 <lin1000@gmail.com>
2018-11-28 16:57:13 +0800

    mode=append

--- app/spark_app_01.py
+++ app/spark_app_01.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     t1.printSchema()
 
     t1DF = t1.select(explode("documents").alias("tweet"))
-    t1DF.select("tweet.FriendsCount","tweet.FollowersCount").write.csv("/output/t1DF")
+    t1DF.select("tweet.FriendsCount","tweet.FollowersCount").write.csv("/output/t1DF", mode="append")
 
-    t1DF.select("tweet.Author","tweet.FollowersCount").write.csv("/output/t1DF-author")
+    t1DF.select("tweet.Author","tweet.FollowersCount").write.csv("/output/t1DF-author", mode="append")
 
     # namesDF = tweetDF.select("firstName","lastName")
     # namesDF.write.option("header","true").csv(sys.argv[2])
