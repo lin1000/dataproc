6b5e8087979b3d189300eb8be5cf5936e4496617
lin1000 <lin1000@gmail.com>
2018-11-28 14:44:09 +0800

    spark_app_01

---
+++ app/spark_app_01.py
@@ -0,0 +1,20 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: spark_app_01.py <input-file> <output-file>")
+        sys.exit()
+
+    spark = SparkSession.builder.getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+    tweetDF = spark.read.json(sys.argv[1])
+    t1 = tweetDF.withColumn("documents",tweetDF.documents)
+    t1.printSchema()
+
+    # namesDF = tweetDF.select("firstName","lastName")
+    # namesDF.write.option("header","true").csv(sys.argv[2])
+
+    spark.stop()
