ca82bc1f14c11ee9ef073cb5b48e06e475945587
lin1000 <lin1000@gmail.com>
2018-11-28 16:50:03 +0800

    remove author column

--- app/spark_app_01.py
+++ app/spark_app_01.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     t1.printSchema()
 
     t1DF = t1.select(explode("documents").alias("tweet"))
-    t1DF.select("tweet.Author","tweet.FollowersCount").write.__file__
+    t1DF.select("tweet.FriendsCount","tweet.FollowersCount").write.csv("/output/t1DF")
 
     # namesDF = tweetDF.select("firstName","lastName")
     # namesDF.write.option("header","true").csv(sys.argv[2])
