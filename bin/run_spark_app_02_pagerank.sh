30d98aec870fb9a2d09334acb13938ef6c7bdc52
lin1000 <lin1000@gmail.com>
2019-02-21 22:25:05 +0800

    catchup

---
+++ bin/run_spark_app_02_pagerank.sh
@@ -0,0 +1,2 @@
+#run_spark_app_02_pagernk.sh
+spark-submit ../app/spark_app_02_pagerank.py /tweet/1.json /tmp/tweet_test_01
---
+++ bin/run_spark_app_03_kmeans.sh
@@ -0,0 +1,2 @@
+#run_spark_app_02_pagernk.sh
+spark-submit ../app/spark_app_03_kmeans.py
